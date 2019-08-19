6b39623b388c6f7dd47e1e3a7d5e8135c3dc775b
Benjamin Earley <benjaminearley@Benjamins-MacBook-Pro-2.local>
2019-08-19 19:02:11 -0400

    Updated Cards.

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,52 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        console.log(response)
+        let cardComponent = response.data.articles.map(category => {
+            for(let i=0; i < data.articles.category.length; i++){
+                return cardCreator(category[i].headline, category[i].authorPhoto, category[i].authorName)
+            }
+        })
+        
+        let cardsContainer = document.querySelector('.cards-container')
+        cardComponent.forEach(newCard => {
+            cardsContainer.appendChild(newCard)
+        })        
+    })
+    .catch(error => {
+        console.log('Error:', error)
+    })
+
+function cardCreator(headline, authorPhoto, authorName){
+        
+    let card = document.createElement('div')
+        card.classList.add('card')
+    
+        let articleHeadline = document.createElement('div')
+            articleHeadline.classList.add('headline')
+            articleHeadline.textContent = headline
+            card.appendChild(articleHeadline)
+    
+        let articleAuthor = document.createElement('div')
+            articleAuthor.classList.add('author')
+            card.appendChild(articleAuthor)
+    
+            let articleImgDiv = document.createElement('div')
+                articleImgDiv.classList.add('img-container')
+                articleAuthor.appendChild(articleImgDiv)    
+    
+                let articleImage = document.createElement('img')
+                    articleImage.setAttribute('src', authorPhoto)
+                    articleImgDiv.appendChild(articleImage)
+            
+            let articleAuthorName = document.createElement('span')
+                articleAuthorName.textContent = `By ${authorName}`
+                articleAuthor.appendChild(articleAuthorName)
+
+    return card
+}
+
+
